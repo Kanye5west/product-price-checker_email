66a2055c0eb1dfc63f8d8987fa8651424dc97a09
mr.Incomplete <153055108+Kanye5west@users.noreply.github.com>
2025-01-09 22:31:18 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+from dotenv import load_dotenv
+import os
+
+url = "https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1"
+headers = {
+    "User-Agent": "user agent of the device",
+    "Accept-language": "accept language of device"
+}
+
+response = requests.get(url= url, headers= headers)
+webp = response.text
+
+soup = BeautifulSoup(webp, "html.parser")
+price = soup.find(class_="a-price-whole").getText()
+price_decimal = soup.find(class_="a-price-fraction").getText()
+product_price = float(price + price_decimal)
+
+load_dotenv()
+
+my_email = os.getenv('GMAIL_MAIL')
+password = os.getenv('PASSWORD_MAIL')
+
+if product_price < 100:
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:  # in order not to use connection.close()
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs="drteves5@gmail.com",
+                            msg=f"Subject:Price Checker!\n\n The price of the Rice cooker is now ${product_price}!"
+                                f" Get Yours now!")
+else:
+    pass
